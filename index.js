dbd8a6160bde6587c31f7697b5771092bd3a46ea
Yebyosh <jappalang@gmail.com>
2024-10-02 13:45:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,39 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+/*
+ A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
+Waiting: 3. A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+*/
 
+app.get("/api/:date", (req, res) => {
+  const {date} = req.params;
+  console.log(`0: ${date}`);
+  console.log(`0': ${Date.parse(date)}`);
+   
+  if (isNaN(Date.parse(date))) {
+    if (Number(date)) {
+      const unix_time = Number(date);
+      const utc_time = new Date(unix_time).toUTCString();
+
+//      console.log(`1: ${unix_time}`);
+//      console.log(`1: ${utc_time}`);
+
+      res.json({unix: unix_time, utc: utc_time});
+    } else {
+      res.json({error : "Invalid Date"});
+    }
+  } else if (new Date(date)) {
+    const unix_time = Date.parse(date);
+    const utc_time = new Date(unix_time).toUTCString();
+
+//    console.log(`3: ${unix_time}`);
+//    console.log(`3: ${utc_time}`);
+
+    res.json({unix: unix_time, utc: utc_time});
+  }
+
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
